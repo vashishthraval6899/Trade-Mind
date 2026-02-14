537aaec59b3df30ecd4b7e8e0ac449a09eb68d98
Vashishth Raval <vraval765@gmail.com>
2026-02-14 17:19:37 +0530

    max_token correction

--- reasoning/llm_client.py
+++ reasoning/llm_client.py
@@ -2,15 +2,14 @@ import requests
 import os
 
 OPENROUTER_API_KEY = os.getenv("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")
-
 OPENROUTER_URL = "https://openrouter.ai/api/v1/chat/completions"
 
-def query_llm(prompt):
+def query_llm(prompt, max_tokens=500, temperature=0.3):
 
     headers = {
         "Authorization": f"Bearer {OPENROUTER_API_KEY}",
-        "HTTP-Referer": "https://trademind.app",   # REQUIRED
-        "X-Title": "TradeMind",                   # REQUIRED
+        "HTTP-Referer": "https://trademind.app",
+        "X-Title": "TradeMind",
         "Content-Type": "application/json"
     }
 
@@ -19,7 +18,8 @@ def query_llm(prompt):
         "messages": [
             {"role": "user", "content": prompt}
         ],
-        "temperature": 0.3
+        "temperature": temperature,
+        "max_tokens": max_tokens
     }
 
     response = requests.post(OPENROUTER_URL, headers=headers, json=data)
